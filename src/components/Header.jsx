387a4375c48298b9873a1065753899e49cf2c5df
Elithon Silva <elithonsilva@gmail.com>
2021-12-25 14:59:16 -0300

    impl: css no componente Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./css/Header.css";
 
 function Header() {
   return (
-    <header>
-      <span>Elithon Silva</span>
-      <nav>
+    <header className="header">
+      <span className="header-title">Elithon Silva</span>
+      <nav className="header-menu">
         <a href="projetos">Projetos</a>
         <a href="contato">Contato</a>
         <a href="sobre">Sobre</a>
