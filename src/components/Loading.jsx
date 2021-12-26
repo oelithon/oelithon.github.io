bf66d40e0bbafaa91c4991a19814616cd0e0061e
Elithon Silva <elithonsilva@gmail.com>
2021-12-26 16:29:55 -0300

    add icon na página Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./css/Loading.css";
+import Construct from "../images/icons/construct-outline.svg";
 
 function Loading() {
   return (
     <section className="section-loading">
+      <img
+        src={Construct}
+        alt="Um ícone de duas ferramentas de forma cruzada."
+        className="icon-size"
+      />
       <h1>Página em desenvolvimento...</h1>
     </section>
   );
