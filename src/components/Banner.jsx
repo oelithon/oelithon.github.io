db7940486dfa88dcc8b2635e6869e8f83d931cfb
Elithon Silva <elithonsilva@gmail.com>
2021-12-26 12:17:19 -0300

    cria componente Banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/Banner.css";
+
+function Banner() {
+  return (
+    <section className="section-banner">
+      <p>Olá, eu me chamo Elithon.</p>
+    </section>
+  );
+}
+
+export default Banner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <section>
         <span>Home</span>
       </section>
