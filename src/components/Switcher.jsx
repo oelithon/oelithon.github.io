3f7c4750802360926b90267acca3d4a14eac089c
Elithon Silva <elithonsilva@gmail.com>
2022-01-02 11:13:52 -0300

    cria página de Projects

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFoundPage from "../pages/NotFoundPage";
 import LoadingPage from "../pages/LoadingPage";
+import Projects from "../pages/Projects";
 
 function Switcher() {
   return (
     <Switch>
+      <Route path="/projects" component={Projects} />
       <Route exact path="/loading" component={LoadingPage} />
       <Route exact path="/" component={Home} />
       <Route exact patch="" component={NotFoundPage} />
