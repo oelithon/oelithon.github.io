841f80f520410221e8100b555fdeb8d5ce772da6
Elithon Silva <elithonsilva@gmail.com>
2022-01-03 09:12:20 -0300

    implementação de css grid

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import GridProjects from "../components/GridProjects";
 
 function Projects() {
   return (
     <>
       <Header />
+      <GridProjects />
     </>
   )
 }
